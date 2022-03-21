8f07a8d441ea20738fc9291b30d9844a7527dc68
P4RZ1V4L-93 <rohit111rahul93@gmail.com>
2022-03-21 17:14:30 +0530

    Initial COmmit

---
+++ marketmarker.py
@@ -0,0 +1,10 @@
+from numpy import source
+from kivy.garden.mapview import MapMarkerPopup
+
+class MarketMarker(MapMarkerPopup):
+    # source = 'part2/marker.png'
+    def on_release(self):
+        '''
+        Open up the LocationPopupMenu
+        '''
+        pass
